de4c3740e93b19270bc3d10a5797098f5647d438
Ashwin0006 <ashcrafts0006@gmail.com>
2023-12-22 09:50:56 +0530

    View Mentee Details Functionality added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 import pickle
 from student import *
 from mentor import *
-
+from flask import jsonify
 
 
 app = Flask(__name__)
@@ -14,7 +14,17 @@ def get_notifications():
     return data
 
 def get_details(name):
-    pass
+    mentee_data = load_from_pickle('student_data.pickle')
+
+    for mentee_obj in mentee_data:
+        if(mentee_obj.id == name or mentee_obj.name == name):
+            name = mentee_obj.name
+            id = mentee_obj.id
+            details = mentee_obj.personal
+            mentor_assigned = mentee_obj.mentor_assigned
+    
+    return name, id, details, mentor_assigned
+    
 
 def save_to_json(path, data):
     with open(path, 'w') as file:
@@ -124,7 +134,7 @@ def login():
                         print("Details :", student_id, student_name, student_details)
 
                 return render_template("main_mentor.html", ids=student_id, 
-                                       names=student_name, details=student_details)
+                                       names=student_name)
             elif(role == "student"):
                 ############ ---------- Work ---------------
                     # Need to create a page called 'main_students.html'
@@ -160,13 +170,35 @@ def schedule_meeting():
 def end_page_schedule_meeting():
     return render_template("go_home_page.html", message="Meeting has been Scheduled!")
 
-@app.route("/view_details", methods=["POST", "GET"])
+@app.route("/view_details", methods=["POST"])
 def view_details():
     data = request.get_json()
     mentee_name = data.get("menteeName")
 
-    details = get_details(mentee_name)
-    return "Details Are Shown!"
+    name, id, details, mentor_assigned = get_details(mentee_name)
+
+    return jsonify(name=name, id=id, details=details, mentor_assigned=mentor_assigned)
+
+@app.route('/details_page')
+def details_page():
+
+    name = request.args.get('name')
+
+    # Fetch details using the get_details function
+    name, id, details, mentor_assigned = get_details(name)
+
+    # Pass the details to the template
+    
+    return render_template(
+        'student_details_template.html',
+        name=name,
+        id=id,
+        details=details,
+        mentor_assigned="Test Runner"
+    )
+    
+   # return jsonify(name, id, details, mentor_assigned)
+
 
 @app.route("/signup_page")
 def signup_page():
