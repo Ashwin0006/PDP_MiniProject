82c300836c128b2da8f2870f548e0d28c92c0677
Ashwin0006 <ashcrafts0006@gmail.com>
2023-12-23 21:21:27 +0530

    2 Design Patterns Added

--- manager_app.py
+++ manager_app.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from reports import *
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def manager_home():
-    return "Hello Manager"
+    return render_template('manager_home.html')
+
+@app.route("/generate_data", methods=["POST", "GET"])
+def generate_data():
+
+    id_no = request.form["student_id"]
+    option = request.form["report"]
+    report = Report(id_no)
+    if(option == "simple"):
+        generated_txt = report.generate()
+    elif(option == "detailed"):
+        report.change_stratergy(Complete_Details)
+        generated_txt = report.generate()
+    else:
+        report.change_stratergy(Every_Details)
+        generated_txt = report.generate()
+
+    return render_template("display_generated_report.html", generated_txt=generated_txt)
 
 
 if(__name__ == "__main__"):
--- reports.py
+++ reports.py
@@ -1,32 +1,66 @@
 import pickle
 import types
-
-
+import json
 
 class Report:
-    def __init__(self, strategy=None):
+    def __init__(self, id_no, strategy=None):
         if(strategy is not None):
+            self.id = id_no
             self.strategy = strategy
-            self.strategy_method = types.MethodType(strategy)
+            self.strategy_method = types.MethodType(strategy, self.id)
         else:
             # Default one!
-            self.strategy = "Complete_Details"
+            self.id = id_no
+            self.strategy = "Simplified_Details"
     
     def generate(self):
         data = self.strategy_method()
         return data
     
     def strategy_method(self):
-        data = Complete_Details()
+        data = Simplfied_Details(self.id)
         return data
     
     def change_stratergy(self, strategy):
         self.strategy = strategy
-        self.strategy_method = types.MethodType(strategy)
+        self.strategy_method = types.MethodType(strategy, self.id)
 
 
 # Stratergy 1
-def Complete_Details():
+def Simplfied_Details(id_no):
+    with open('mentor_mentee.pickle', 'rb' ) as infile:
+        mentor_mentee_data = pickle.load(infile)
+
+    # Generating Report
+    for mentor in mentor_mentee_data:
+        for stud_obj in mentor_mentee_data[mentor]:
+            if(stud_obj.id == id_no or stud_obj.name == id_no):
+                mentor_name = mentor.name
+                student_name = stud_obj.name
+                student_details = stud_obj.personal
+                reports_data = "-" * 40 + "<br>" + f"Student Name: {student_name}<br>Student Id: {stud_obj.id}<br>Mentor Name: {mentor_name}<br>Details: {student_details}<br>"
+
+    
+    return reports_data
+
+#Stratergy 2
+def Complete_Details(id_no):
+
+    with open('students_data_form.json', 'r' ) as infile:
+        student_data = json.load(infile)
+    
+    for data in student_data:
+        if(data["id_no"] == id_no):
+            reports_data = data
+    
+    string = ""
+    for k,v in reports_data.items():
+        string += f"{k} :{v}" + "<br>"
+    
+    return string
+
+# Stratergy 3
+def Every_Details(id_no):
     # Define a helper Method
     def get_full_detail(obj, req_data):
         data = None
@@ -36,7 +70,7 @@ def Complete_Details():
                     mentor = k.name
                     student_name = obj.name
                     student_details = obj.personal
-                    data = "-" * 40 + "\n" + f"Student Name :{student_name}\nStudent Id :{obj.id}\nMentor Name :{mentor}\nDetails :{student_details}\n" 
+                    data = "-" * 40 + "<br>" + f"Student Name :{student_name}<br>Student Id :{obj.id}<br>Mentor Name :{mentor}<br>Details :{student_details}<br>" 
 
         return data
 
@@ -46,8 +80,6 @@ def Complete_Details():
     with open('mentor_mentee.pickle', 'rb' ) as infile:
         mentor_mentee_data = pickle.load(infile)
 
-    print(student_data)
-    print(mentor_mentee_data)
 
     # Generating Report
     reports_data = ""
@@ -55,8 +87,4 @@ def Complete_Details():
         reports_data += get_full_detail(student_obj, mentor_mentee_data)
     
     return reports_data
-
-if __name__ == "__main__":
-    r1 = Report()
-    # Using Statergy 1
-    print(r1.generate())
+    
